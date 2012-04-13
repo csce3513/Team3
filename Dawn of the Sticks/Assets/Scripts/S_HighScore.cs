b2217a2124bd78e554d1a9e3b01091fecffd060c
Thao <mia2709@gmail.com>
2012-04-12 19:57:07 -0500

    Sorted HighScore code

--- Dawn of the Sticks/Assets/Scripts/S_HighScore.cs
+++ Dawn of the Sticks/Assets/Scripts/S_HighScore.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System.Collections;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.IO;
+using System;
 
 public class S_HighScore : MonoBehaviour
 {
@@ -11,12 +13,10 @@ public class S_HighScore : MonoBehaviour
 	public string fileScore;
 	static public string textInformation;
 	static public string ScoreValue;
-//	bool save = false;
-	//bool overwrite = false;
 	bool back = false;
-	//int increSave = 0;
 	static private string Name;
 	static private float score;
+	private Vector2 scrollViewVector = Vector2.zero;
 
 	static public string UpdateName(string name)
 	{
@@ -26,9 +26,10 @@ public class S_HighScore : MonoBehaviour
    
 	static public float UpdateScore ()
 	{
-		score = S_Player.playerscore;
+		score = S_Player.playerscore;//<---------Change S_Player.zombieskilled to whatever variable that you want it to be score
 		return score;
 	}
+    
 	// Use this for initialization
 	void Start ()
 	{
@@ -37,33 +38,27 @@ public class S_HighScore : MonoBehaviour
 		fileScore = "/fileScore.txt";
 		textInformation = "";
 		ScoreValue = "";
+		Fetch_Sort();
 	}
-	private Vector2 scrollViewVector = Vector2.zero;
-	//private string innerText = "I am inside the ScrollView";
-
 
 	void OnGUI()
 	{
 		// Begin the ScrollView
-		scrollViewVector = GUI.BeginScrollView (new Rect (0.125f*Screen.width,
-		                                        0.4f*Screen.height, 0.75f*Screen.width,
+		scrollViewVector = GUI.BeginScrollView (new Rect (0.35f*Screen.width,
+		                                        0.4f*Screen.height, 0.7f*Screen.width,
 		                                        0.3f*Screen.height), scrollViewVector, 
-		                                        new Rect (0.125f*Screen.width,0.4f*Screen.height,
-		                                                  0.7f*Screen.width, .6f*Screen.height));
+		                                        new Rect (0.3f*Screen.width,0.4f*Screen.height,
+		                                                  0.3f*Screen.width, .6f*Screen.height));
 
 		// Put something inside the ScrollView
-		textInformation = GUI.TextArea(new Rect(0.25f*Screen.width,0.4f*Screen.height,
-		                          0.25f*Screen.width,0.6f*Screen.height),textInformation);
-		ScoreValue = GUI.TextArea(new Rect(0.5f*Screen.width,0.4f*Screen.height,
-		                          0.25f*Screen.width,0.6f*Screen.height),ScoreValue);
+		textInformation = GUI.TextArea(new Rect(0.35f*Screen.width,0.4f*Screen.height,
+		                          0.3f*Screen.width,0.6f*Screen.height),textInformation);
+		
 		// End the ScrollView
 		GUI.EndScrollView();
 
-		GUI.Box(new Rect(0.3f*Screen.width,0.3f*Screen.height,
-		                 100,20), "NAME");
-		//GUI.Label(new Rect(0.25f*Screen.width,0.4f*Screen.height, 0.1f*Screen.width , 0.04f*Screen.height), "NAME");
-		GUI.Box(new Rect(0.575f*Screen.width,0.3f*Screen.height,
-		                 100,20), "SCORE");
+		GUI.Box(new Rect(0.425f*Screen.width,0.3f*Screen.height,
+		                 150,20), "NAME : SCORE ");
 		//GUI.Label(new Rect(0.85f*Screen.width,0.4f*Screen.height, 0.1f*Screen.width , 0.04f*Screen.height), "SCORE");
 		if(GUI.Button(new Rect(0.425f*Screen.width,0.8f*Screen.height,
 		                       0.15f*Screen.width,20), "MAIN MENU"))
@@ -72,16 +67,82 @@ public class S_HighScore : MonoBehaviour
 		}
 
 	}
+	
+	static int Compare2(KeyValuePair<string, float> a, KeyValuePair<string, float> b)
+    {
+	    return a.Value.CompareTo(b.Value);
+    }
+
+// Fetching what we got from file then sort	
+	void Fetch_Sort()
+	{
+		var d = new Dictionary<string, float>();
+		int counter = 0;
+		string[] myString = File.ReadAllLines(path+fileName);
+//		File.Delete(path+fileName);
+		
+		foreach (string s in myString)
+			counter++;
+	// 1.Add element to the dictionary	
+        for (int i=0; i<(counter/2);i++)
+		{
+		   d.Add(myString[i*2], float.Parse(myString[i*2 + 1]));		
+		}
+	// 2.Order by values.
+	// Use LINQ to specify sorting by value.
+	    var items = from k in d.Keys
+	                orderby d[k] descending
+	                select k;
+
+	// 3. Display results.
+	foreach (string k in items)
+	{
+	    textInformation += (k + " : "+ d[k] + "\n");
+	}
+		
+		
+//		textInformation = aList[0].Key.ToString()+"\n" + aList[1].Key.ToString()+"\n"+aList[2].Key.ToString()+"\n"+aList[3].Key.ToString() + "\n";
+//		textInformation = aList[0].Key.ToString()+"\n"
+//			            + aList[1].Key.ToString()+"\n"
+//				        + aList[2].Key.ToString()+"\n";
+//				        + aList[3].Key.ToString()+"\n"
+//				 +aList[4].Key.ToString()+"\n"
+//				 +aList[5].Key.ToString()+"\n"
+//				 +aList[6].Key.ToString()+"\n"
+//				 +aList[7].Key.ToString()+"\n"
+//				 +aList[8].Key.ToString()+"\n";
+		
+	}
+	
 	// Update is called once per frame
 	void Update ()
 	{
-			using(FileStream fs = File.OpenRead(path + fileName))
+		
+		//string temp1 = "", temp2 = "" ;
+		// Read the file and display it line by line.
+        //System.IO.StreamReader file = new System.IO.StreamReader(path+fileName);
+		
+//		textInformation = aList.Count.ToString();
+//		textInformation = aList[0].Key.ToString()+"\n"+aList[1].Key.ToString()+"\n"+aList[2].Key.ToString()+"\n"+aList[3].Key.ToString() + "\n";
+//		for(int i=0; i < 3; i++)
+//	{
+//			if(File.Exists(path + fileScore))
+//			{
+//				using(StreamWriter fs = File.AppendText(path + fileScore))
+//				{
+//				    fs.WriteLine(aList[i].Value.ToString()+"\n");	
+//				}
+//			}
+//	}
+
+/*			using(FileStream fs = File.OpenRead(path + fileName))
 			{
 			   byte[] b = new byte[1024];
 			   UTF8Encoding temp = new UTF8Encoding(true);
 			   while(fs.Read(b, 0 ,b.Length)>0)
 			   {
-			       textInformation = temp.GetString(b);				   
+			       textInformation = temp.GetString(b);
+				   //aList.Add(new KeyValuePair<string, float>(temp.GetString(b), 1));
 			   }
 			}
 		    using(FileStream fs = File.OpenRead(path + fileScore))
@@ -93,9 +154,15 @@ public class S_HighScore : MonoBehaviour
 			       ScoreValue = temp.GetString(b);				   
 			   }
 			}
+*/
 		if (back)
 		{
 			Application.LoadLevel("MainMenu");
 		}
+	}
+	public void AddText(FileStream fs, string value)
+	{
+		byte[] info = new UTF8Encoding(true).GetBytes(value);
+		fs.Write(info, 0, info.Length);
 	}
 }
